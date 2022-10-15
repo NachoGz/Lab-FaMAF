d2d82df9d31e5a650318f6a5e173b42408d17e91
Ignacio Gomez <nachogomez8gf@gmail.com>
2022-10-15 19:25:06 +0000

    Update ejercicio4a.c

--- AYED/proyecto3/ejercicio4a.c
+++ AYED/proyecto3/ejercicio4a.c
@@ -11,20 +11,13 @@ int main()
     // Solicito valor para y
     printf("Ingrese un valor inicial para y:\n");
     scanf("%d",&y);
-    
-    if (x >= y){
-        x = 0;
-    }
-    else {
-        x = 2;
-    }
-    printf("Valor final de x:%d\n", x);
-    printf("Valor final de y:%d\n", y);
-    
-// Traducción del programa 1.f
+    #include <stdio.h>
+// Traducción del programa 1.e
+int main()
+{
     int x;
     int y;
-    
+    printf("Traducción del programa 1.e\n");
     // Solicito valor para x
     printf("Ingrese un valor inicial para x:\n");
     scanf("%d",&x);
@@ -38,7 +31,25 @@ int main()
     else {
         x = 2;
     }
-    printf("Valor final de x:%d\n", x);
-    printf("Valor final de y:%d\n", y);
+    printf("Valor final de x: %d\n", x);
+    printf("Valor final de y: %d\n\n", y);
+    
+// Traducción del programa 1.f
+    printf("Traducción del programa 1.f\n");
+    // Solicito valor para x
+    printf("Ingrese un valor inicial para x:\n");
+    scanf("%d",&x);
+    // Solicito valor para y
+    printf("Ingrese un valor inicial para y:\n");
+    scanf("%d",&y);
+    
+    if (x >= y){
+        x = 0;
+    }
+    else {
+        x = 2;
+    }
+    printf("Valor final de x: %d\n", x);
+    printf("Valor final de y: %d\n", y);
     return 0;
 }
