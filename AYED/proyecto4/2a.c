f4a4ab8ce89a3884dffda63ae5f25946d702adf7
Ignacio Gomez <nachogomez8gf@gmail.com>
2022-10-23 21:55:22 +0000

    Add files via upload

---
+++ AYED/proyecto4/2a.c
@@ -0,0 +1,19 @@
+int main()
+{
+    int x,y,z;
+
+    // Solicito valor de x
+    printf("Ingrese el valor de x: \n");
+    scanf("%d", &x);
+
+    // Solicito valor de y
+    printf("Ingrese el valor de y: \n");
+    scanf("%d", &y);
+
+    // Asignación
+    z = x;
+    x = x + 1;
+    y = x + y
+    
+    return 0;
+}
