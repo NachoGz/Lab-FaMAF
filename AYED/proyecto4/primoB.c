ba5e5d6141b15c3f7e8a50905598ab40740b7e16
Ignacio Gomez <nachogomez8gf@gmail.com>
2022-10-24 16:47:11 -0300

    Create primoB.c

---
+++ AYED/proyecto4/primoB.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+bool esPrimo(int n)
+{
+    int i = 2; 
+    bool primo=true;
+    while (i<n)
+    {
+        if ((n%i)!=0){
+            primo=true;
+        }
+        else{
+            return false;
+        }
+        i += 1;
+    }
+    return primo;
+
+}
+
+int nesimo_primo(int N)
+{   
+    int i=2, k=1, primo=0;
+    while (k<=N)
+    {
+        if (esPrimo(i))
+        {
+            k += 1;
+            primo = i;
+        }
+        i += 1;
+    }
+    return primo;
+}
+
+
+int main()
+{
+    int n, primo;
+    printf("Ingrese un número entero mayor a 0: \n");
+    scanf("%d", &n);
+    while (n<0)
+    {   
+        printf("El número entero debe ser mayor a 0. Ingrese otro número.\n");
+        scanf("%d", &n);
+    }
+    
+    primo = nesimo_primo(n);
+    printf("El numero primo número %d es: %d\n", n, primo);
+    return 0;
+}
