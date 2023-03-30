70b53a8536ec221bfdee889bdc0084ea514c9d9c
Ignacio Gomez <nachogomez8gf@gmail.com>
2023-03-30 12:38:59 -0300

    Update array_helpers.c

--- ayed2/lab01/ej3/array_helpers.c
+++ ayed2/lab01/ej3/array_helpers.c
@@ -4,6 +4,61 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-unsigned int length = array_from_file(array, MAX_SIZE, filepath);
-/*dumping the array*/
-array_dump(array, length);
+unsigned int array_from_file(int array[],
+                             unsigned int max_size,
+                             const char *filepath) {
+    //your code here!!!
+    FILE* file;
+    unsigned int length;
+    //abro el archivo
+    file = fopen(filepath, "r");
+
+    // itero cada caracter
+    for (unsigned int i=0;i<max_size;i++)
+    {   
+        // indicador EOF
+        if (feof(file))
+        {
+            break;
+        }
+        // busco el largo del array
+        if (i==0)
+        {
+            fscanf(file, "%u", &length);
+        }
+        else
+        {
+            fscanf(file, "%d", &array[i-1]);
+        }
+    }
+    fclose(file);
+    return length;
+}
+
+void array_dump(int a[], unsigned int length) {
+    //your code here!!!
+    printf("\n[ ");
+    for (unsigned int i=0;i<length;i++)
+    {   
+        if (i==(length-1))
+        {
+            printf("%d ", a[i]);
+        }
+        else
+        {
+            printf("%d, ", a[i]);       }
+    }
+    printf("]\n");
+}
+
+bool array_is_sorted(int a[], unsigned int length)
+{
+    bool sorted = true;
+    for (unsigned int i=0; i<length-1; i++)
+    {
+        sorted = sorted && (a[i] <= a[i+1]);
+    }
+    return sorted;
+}
+
+
