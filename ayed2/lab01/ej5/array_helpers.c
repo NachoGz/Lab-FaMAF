c4a86827b4ec5f526c939678c25eab9bf9653558
Ignacio Gomez <nachogomez8gf@gmail.com>
2023-03-30 12:41:11 -0300

    Add files via upload

---
+++ ayed2/lab01/ej5/array_helpers.c
@@ -0,0 +1,64 @@
+#include "array_helpers.h"
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "mybool.h"
+
+unsigned int array_from_file(int array[],
+                             unsigned int max_size,
+                             const char *filepath) {
+    //your code here!!!
+    FILE* file;
+    unsigned int length;
+    //abro el archivo
+    file = fopen(filepath, "r");
+
+    // itero cada caracter
+    for (unsigned int i=0;i<max_size;i++)
+    {   
+        // indicador EOF
+        if (feof(file))
+        {
+            break;
+        }
+        // busco el largo del array
+        if (i==0)
+        {
+            fscanf(file, "%u", &length);
+        }
+        else
+        {
+            fscanf(file, "%d", &array[i-1]);
+        }
+    }
+    fclose(file);
+    return length;
+}
+
+void array_dump(int a[], unsigned int length) {
+    //your code here!!!
+    printf("\n[ ");
+    for (unsigned int i=0;i<length;i++)
+    {   
+        if (i==(length-1))
+        {
+            printf("%d ", a[i]);
+        }
+        else
+        {
+            printf("%d, ", a[i]);       }
+    }
+    printf("]\n");
+}
+
+mybool array_is_sorted(int a[], unsigned int length)
+{
+    mybool sorted = true;
+    for (unsigned int i=0; i<length-1; i++)
+    {
+        sorted = sorted && (a[i] <= a[i+1]);
+    }
+    return sorted;
+}
+
+
---
+++ ayed2/lab01/ej5/array_helpers.h
@@ -0,0 +1,14 @@
+#include "mybool.h"
+#ifndef arrray_helpers_h
+#define array_helpers_h
+
+
+unsigned int array_from_file(int array[],
+                             unsigned int max_size,
+                             const char *filepath); 
+
+void array_dump(int a[], unsigned int length);
+
+mybool array_is_sorted(int a[], unsigned int length);
+
+#endif
