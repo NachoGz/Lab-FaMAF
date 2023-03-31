3160d986cd02e2085b7da253db4e501445c80318
Ignacio Gomez <nachogomez8gf@gmail.com>
2023-03-31 17:18:19 -0300

    Update array_helpers.h

--- ayed2/lab01/ej3/array_helpers.h
+++ ayed2/lab01/ej3/array_helpers.h
@@ -1,5 +1,4 @@
 #include <assert.h>
-#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -9,11 +8,3 @@ unsigned int array_from_file(int array[],
 
 void array_dump(int a[], unsigned int length);
 
-bool array_is_sorted(int a[], unsigned int length);
-/*
-    Dado un array a[] y su tamaño length debe devolver
-    true si y sólo si los elementos del arreglo a[] 
-    están ordenados de manera ascendente, es decir si:
-    a[0] <= a[1] <= ... <= a[length - 1]
-
-*/
