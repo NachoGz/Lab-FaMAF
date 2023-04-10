2fc4fc1a8f8d828eccb170cc1d630efdc035bb5d
Ignacio Gomez <nachogomez8gf@gmail.com>
2023-04-10 19:55:02 -0300

    Add files via upload

--- ayed2/lab02/ej1/sort.c
+++ ayed2/lab02/ej1/sort.c
@@ -10,17 +10,24 @@
 // gcc -Wall -Werror -Wextra -pedantic -std=c99 -no-pie array_helpers.o sort.o sort_helpers.o main.o -o sorter
 
 static void insert(int a[], unsigned int i) {
-    i = 1;
-    while (i > 1 && a[i] < a[i-1])
+    int j;
+    j = i;
+
+    while (j > 0 && a[j] < a[j-1])
     {
-        swap(a, i-1, i);
-        i -= 1;
+        swap(a, j-1, j);
+        j -= 1;
     }
+    
 }
+
 
 void insertion_sort(int a[], unsigned int length) {
     for (unsigned int i = 1u; i < length; ++i) {
-        assert(array_is_sorted(a, length));
         insert(a, i);
+        // assert(array_is_sorted(a, i));
     }
 }
+
+// falta parte b
+
