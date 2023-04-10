6e07caee652959194964e4d19065a91c00e855de
Ignacio Gomez <nachogomez8gf@gmail.com>
2023-04-10 20:35:14 -0300

    Add files via upload

---
+++ ayed2/lab02/ej3/sort.c
@@ -0,0 +1,62 @@
+#include <assert.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+#include "array_helpers.h"
+#include "sort_helpers.h"
+#include "sort.h"
+
+
+static unsigned int partition(int a[], unsigned int izq, unsigned int der) {
+    /* Needs implementation */
+
+    /* Permutes elements of a[izq..der] and returns pivot such that:
+     - izq <= pivot <= der
+     - elements in a[izq,pivot) all 'go_before' (according to function goes_before) a[pivot]
+     - a[pivot] 'goes_before' all the elements in a(pivot,der]
+    */
+    int i, j, ppiv;
+    ppiv = izq;
+    i = izq + 1;
+    j = der;
+
+    while (i <= j)
+    {
+        if (a[i] <= a[ppiv])
+        {
+            i += 1;
+        }
+        else
+        {
+            if (a[j] >= a[ppiv])
+            {
+                j -= 1;
+            }
+            else
+            {
+                if (a[i] > a[ppiv] && a[j] < a[ppiv])
+                {
+                    swap(a,i,j);
+                }
+            }
+        }
+    }
+    swap(a, ppiv, j);
+    return j;
+}
+
+static void quick_sort_rec(int a[], unsigned int izq, unsigned int der) {
+    unsigned int ppiv;
+    if (der > izq)
+    {   
+        ppiv = partition(a, izq, der);
+        quick_sort_rec(a, izq, ppiv-1);
+        quick_sort_rec(a, ppiv+1, der);
+    }
+}
+
+void quick_sort(int a[], unsigned int length) {
+    quick_sort_rec(a, 0, (length == 0) ? 0 : length - 1);
+}
+
