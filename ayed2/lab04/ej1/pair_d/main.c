df4b3e7025c8bdf4e6cdb325a1e30c0fd8f41fec
Ignacio Gomez <nachogomez8gf@gmail.com>
2023-05-11 20:48:42 -0300

    Add files via upload

--- ayed2/lab04/ej1/pair_d/main.c
+++ ayed2/lab04/ej1/pair_d/main.c
@@ -28,3 +28,10 @@ int main(void) {
     q = pair_destroy(q);
     return EXIT_SUCCESS;
 }
+
+/*
+Si hay encapsulamiento porque no puede acceder a la representación interna del TAD, ya que
+se uso typedef struct s_pair_t * pair_t. Esto significa que hay un puntero que apunta al struct.
+Con eso, C ya puede compilar bien porque sabe cuanta memoria guardar para ese archivo. Depende del programador que importe
+esa librería definir correctamente el TAD.
+*/
